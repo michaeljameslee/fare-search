796e4cb76495e06973ef03ea420907c8ea897cc9
Mike Lee <mike@mvpindex.com>
2016-08-13 14:34:45 -0500

    Header and responsive

--- app/models/fareSearch.js
+++ app/models/fareSearch.js
@@ -4,18 +4,22 @@ angular.module('myApp.fareSearch', ['ngRoute'])
 
 .factory('fareSearch', ['api', function fareSearch(api) {
 
-	var d = new Date(),
-		today = d.getTime();
+	var fareSearch = {};
 
-	var fareSearch = {
+	fareSearch.searchParams = {
 		origin: 'DEN',
 		destination: 'BOS',
-		departureDate: today,
-		returnDate: today + 12096e5
+		departureDate: moment().add(1, 'days').format('l'),
+		returnDate: moment().add(15, 'days').format('l')
 	};
 
 	fareSearch.searchFares = api.searchFares;
 	fareSearch.getCityCodes = api.getCityCodes;
+
+	fareSearch.dateFormat = '';
+
+
+
 
 	return fareSearch;
 
